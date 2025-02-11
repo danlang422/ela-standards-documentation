038eb039c0ca6ea915f9fadf06a27c8e5b43d880
Daniel Lang <danslang@gmail.com>
2025-02-10 20:11:44 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import StandardDocumentation from '../components/StandardDocumentation';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Standards Documentation</h1>
+      <StandardDocumentation />
+    </div>
+  );
+}
