4de5d94541e5fc4d9f392385a59cce59d9a45333
Daniel Lang <danslang@gmail.com>
2025-02-11 13:39:21 -0600

    Update configuration for GitHub Pages

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold">ELA Standards Documentation</h1>
-      <p className="mt-4">Welcome to the standards documentation system.</p>
-    </main>
-  )
-}
--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import StandardsViewer from '../components/StandardsViewer'
-
-export default function Home() {
-  return (
-    <div>
-      <StandardsViewer />
-    </div>
-  )
-}
