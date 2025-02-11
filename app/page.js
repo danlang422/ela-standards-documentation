37f1df44a902aacdc522cd2246300200c1459336
Daniel Lang <danslang@gmail.com>
2025-02-11 13:22:17 -0600

    Add simple test page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
-import StandardsViewer from '../components/StandardsViewer'
-
 export default function Home() {
   return (
     <main className="container mx-auto p-4">
-      <StandardsViewer />
+      <h1 className="text-4xl font-bold">ELA Standards Documentation</h1>
+      <p className="mt-4">Welcome to the standards documentation system.</p>
     </main>
   )
 }
