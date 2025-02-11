d8b2e3f2308ec6cc970033ec6df02723f0b5528c
Daniel Lang <danslang@gmail.com>
2025-02-11 13:37:02 -0600

    Update next config for local static serving

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/ela-standards-documentation',
+  // The basePath is only needed for GitHub Pages deployment, not local development
+  // basePath: process.env.NODE_ENV === 'production' ? '/ela-standards-documentation' : '',
   images: {
     unoptimized: true,
-  }
+  },
+  assetPrefix: './',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
